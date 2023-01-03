4c43f2f1e2db9a49e367d66130bbc225ebfdecb1
nursultanzhumashov <nursultanzhumashov0@gmail.com>
2023-01-03 15:25:40 +0600

    refoctoring

--- lib/app/screens/aut/authentication_screen.dart
+++ lib/app/screens/aut/authentication_screen.dart
@@ -13,52 +13,51 @@ class AuthenticationScreen extends StatelessWidget {
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(3),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    'https://picsum.photos/200/300',
-                    height: 112,
-                    width: 120,
-                  ),
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  'https://picsum.photos/200/300',
+                  height: 112,
+                  width: 120,
                 ),
-                const Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Сделай жизнь проще',
-                    style: TextStyle(fontSize: 18),
-                  ),
+              ),
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Сделай жизнь проще',
+                  style: TextStyle(fontSize: 18),
                 ),
-                const AuthenticationWidget(),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomePage2()),
-                    );
-                  },
-                  child: Title(
-                    color: Colors.red,
-                    child: const Text('Camera,gallery '),
-                  ),
+              ),
+              const AuthenticationWidget(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage2()),
+                  );
+                },
+                child: Title(
+                  color: Colors.red,
+                  child: const Text('Camera,gallery '),
                 ),
-                const SizedBox(height: 10),
-                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>  SuggestionPage()),
-                    );
-                  },
-                  child: Title(
-                    color: Colors.red,
-                    child: const Text('Suggestion'),
-                  ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SuggestionPage()),
+                  );
+                },
+                child: Title(
+                  color: Colors.red,
+                  child: const Text('Suggestion'),
                 ),
-              ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
